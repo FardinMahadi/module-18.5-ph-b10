7b16de18fde43d1cb20703f978677439ee2383ef
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-08-30 21:25:30 +0600

    done

--- main.js
+++ main.js
@@ -41,3 +41,21 @@ friends.shift();
 console.log(friends);
 friends.unshift("Fabul");
 console.log(friends);
+
+// isArray
+console.log(friends.includes("Babul"));
+
+// indexOf
+console.log(friends.indexOf("Cabul"));
+// absent
+console.log(friends.indexOf("cabul"));
+
+const nums = [];
+const food = "roso o gol l a";
+console.log(Array.isArray(friends));
+console.log(Array.isArray(nums));
+console.log(Array.isArray(food));
+console.log(Array.isArray(ages));
+
+// join
+console.log(friends.join(" | "));
